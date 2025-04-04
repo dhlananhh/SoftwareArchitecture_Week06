80f8fa42fd05351a138c44cfec56a8d7b6630a8f
Lan Anh <dhlananh2309@gmail.com>
2025-04-04 19:06:06 +0700

    feat: add .gitignore, update Dockerfile and app for Flask, and enhance requirements

--- Exercise02_PythonFlask/app.py
+++ Exercise02_PythonFlask/app.py
@@ -3,8 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_docker_flask():
-    return 'Hello, Docker Flask!'
+def hello():
+    return "Hello, Docker Flask!\n"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    # Chạy server trên địa chỉ 0.0.0.0 để có thể truy cập từ bên ngoài container
+    app.run(host='0.0.0.0', port=5000, debug=True) # debug=True chỉ dùng cho developer
